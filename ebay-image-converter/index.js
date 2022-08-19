0d8d7ffc8a44fd9754418b667ca4cca166c50f5d
LittleSanchez <hlipavka2004@gmail.com>
2022-08-19 08:14:24 +0300

    image parser and conirmator fixes

--- ebay-image-converter/index.js
+++ ebay-image-converter/index.js
@@ -1,165 +1,188 @@
-const { Axios, default: axios } = require('axios');
-const { exec } = require('child_process');
+const { Axios, default: axios } = require("axios");
+const { exec } = require("child_process");
 
-const express = require('express')
-const cors = require('cors');
-const { createHash } = require('crypto');
-const bodyParser = require('body-parser');
-const { OUT_DIR, transferLogo, WORK_DIR, getFileExtension } = require('./logo-transfer');
-const { createWriteStream, rm, rmSync } = require('fs');
+const express = require("express");
+const cors = require("cors");
+const { createHash } = require("crypto");
+const bodyParser = require("body-parser");
+const {
+    OUT_DIR,
+    transferLogo,
+    WORK_DIR,
+    getFileExtension,
+    withWindowsPath,
+} = require("./logo-transfer");
+const { createWriteStream, rm, rmSync } = require("fs");
 
-console.log(new Date().toISOString())
-
+console.log(new Date().toISOString().replace(/:/g, '-'));
 
 const VERIFICATION_TOKEN = "t9v-t7i8o7_tiu-r7ihl37iGLg_L43423";
-const ENDPOINT = "https://c46d-90-138-230-168.ngrok.io";
-
+const ENDPOINT = "https://7561-90-138-121-152.ngrok.io";
 
 async function downloadFile(fileUrl, outputLocationPath) {
     const writer = createWriteStream(outputLocationPath);
 
-    return axios.get(fileUrl, {
-        responseType: 'stream',
-    }).then(response => {
+    return axios
+        .get(fileUrl, {
+            responseType: "stream",
+        })
+        .then((response) => {
+            //ensure that the user can call `then()` only when the file has
+            //been downloaded entirely.
 
-        //ensure that the user can call `then()` only when the file has
-        //been downloaded entirely.
-
-        return new Promise((resolve, reject) => {
-            response.data.pipe(writer);
-            let error = null;
-            writer.on('error', err => {
-                error = err;
-                writer.close();
-                reject(err);
-            });
-            writer.on('close', () => {
-                if (!error) {
-                    resolve(true);
-                }
-                //no need to call the reject here, as it will have been called in the
-                //'error' stream;
+            return new Promise((resolve, reject) => {
+                response.data.pipe(writer);
+                let error = null;
+                writer.on("error", (err) => {
+                    error = err;
+                    writer.close();
+                    reject(err);
+                });
+                writer.on("close", () => {
+                    if (!error) {
+                        resolve(true);
+                    }
+                    //no need to call the reject here, as it will have been called in the
+                    //'error' stream;
+                });
             });
         });
-    });
 }
 
 const proceedImages = async (companyName, images) => {
-    const __dir = `${companyName}-${new Date().toISOString()}`;
-    const outputDir = `${OUT_DIR}/${__dir}`;
+    const __dir = `${companyName}-${new Date().toISOString().replace(/:/g, '-')}`;
+    const outputDir = withWindowsPath(`${OUT_DIR}/${__dir}`);
     exec(`mkdir ${outputDir}`, (err, stdout, stderr) => {
         if (err) {
-            console.error(err)
+            console.error(err);
         } else {
             console.log(`stdout: ${stdout}`);
             console.log(`stderr: ${stderr}`);
         }
-    })
-    exec(`mkdir ${WORK_DIR}/tmp`, (err, stdout, stderr) => {
+    });
+    exec(withWindowsPath(`mkdir ${WORK_DIR}/tmp`), (err, stdout, stderr) => {
         if (err) {
-            console.error(err)
+            console.error(err);
         } else {
             console.log(`stdout: ${stdout}`);
             console.log(`stderr: ${stderr}`);
         }
-    })
+    });
     const newImages = [];
     for (let i = 0; i < images.length; i++) {
         const image = images[i];
 
         console.log("Currently working on ", image);
         const ext = getFileExtension(image);
-        const inputFile = `${WORK_DIR}/tmp/tmp-dwnld-${i + 1}.${ext}`;
-        const outputFile = `${outputDir}/${companyName}-${i + 1}.${ext}`;
+        const inputFile = withWindowsPath(`${WORK_DIR}/tmp/tmp-dwnld-${i + 1}.${ext}`);
+        const outputFile = withWindowsPath(`${outputDir}/${companyName}-${i + 1}.${ext}`);
         try {
             rmSync(inputFile);
-        } catch (e) { }
+        } catch (e) {}
         await downloadFile(image, inputFile);
         await transferLogo(inputFile, companyName, outputFile);
         console.log("Success");
-        newImages.push(`${ENDPOINT}/images/${__dir}/${companyName}-${i + 1}.${ext}`);
+        newImages.push(
+            `${ENDPOINT}/images/${__dir}/${companyName}-${i + 1}.${ext}`
+        );
     }
     try {
-        rmSync(`${WORK_DIR}/tmp`);
-    } catch (e) { }
+        rmSync(withWindowsPath(`${WORK_DIR}/tmp`));
+    } catch (e) {}
     return newImages;
-}
+};
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: '*'
-}));
+const corsOptions = {origin: '*'}
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 
-app.use('/images', express.static(__dirname + '/out'));
+app.use("/images", express.static(withWindowsPath(__dirname + "/out")));
 
 const port = 3253;
 
-app.post('/', async (req, res) => {
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // restrict it to the required domain
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    // Set custom headers for CORS
+    res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
+
+    console.log("ADDED CORS HEADERS: ", res.headersSent)
+    if (req.method === "OPTIONS") {
+        console.log("PREFLIGHT OPTIONS METHOD");
+        res.send({});
+        console.log("ADDED CORS HEADERS After: ", res.headersSent)
+        return res.end();
+    }
+    
+    return next();
+})
+
+app.use(cors(corsOptions))
+
+app.post("/", async (req, res) => {
     console.log(req.body);
     const { companyName, images } = req.body;
-    if (!images || !companyName) res.sendStatus(200);
+    if (!images || !companyName) {
+        res.send({});
+        return;
+    }
     const newImages = await proceedImages(companyName, images);
     res.send(newImages);
 });
 
-
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
     const challengeCode = req.query.challenge_code;
     if (!challengeCode) {
-        res.send({})
+        res.send({});
         return;
     }
     console.log(challengeCode);
-    const hash = createHash('sha256');
+    const hash = createHash("sha256");
     hash.update(challengeCode);
     hash.update(VERIFICATION_TOKEN);
     hash.update(ENDPOINT);
-    const responseHash = hash.digest('hex');
+    const responseHash = hash.digest("hex");
     console.log({
-        challengeResponse: new Buffer.from(responseHash).toString()
-    })
+        challengeResponse: new Buffer.from(responseHash).toString(),
+    });
     res.json({
-        challengeResponse: new Buffer.from(responseHash).toString()
-    })
-})
+        challengeResponse: new Buffer.from(responseHash).toString(),
+    });
+});
 
-app.get('/accepted', async (req, res) => {
+app.get("/accepted", async (req, res) => {
     console.log(req.body, req.query);
-    res.redirect('https://www.ebay.com');
-})
+    res.redirect("https://www.ebay.com");
+});
 
-app.post('/accepted', async (req, res) => {
+app.post("/accepted", async (req, res) => {
     console.log(req.body, req.query);
-    res.redirect('https://www.ebay.com');
-})
+    res.redirect("https://www.ebay.com");
+});
 
-app.post('/redirect', async (req, res) => {
+app.post("/redirect", async (req, res) => {
     console.log(req.body);
     try {
-
         const response = await axios({
             url: req.body.url,
             method: req.body.method,
             data: req.body.data,
             params: req.query,
-            headers: req.body.headers
+            headers: req.body.headers,
         });
         res.json({
             status: response.status,
             statusText: response.statusText,
-            data: response.data
+            data: response.data,
         });
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e?.response?.data?.errors);
         res.json(e?.response?.data);
     }
-})
-
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
--- ebay-image-converter/logo-transfer.js
+++ ebay-image-converter/logo-transfer.js
@@ -1,48 +1,47 @@
 const sharp = require("sharp");
-const config = require("./configs")
+const config = require("./configs");
 
 const WORK_DIR = "bin";
-const LOGO = 'logo.png';
+const LOGO = "logo.png";
 
-const COVERING_FILE_NAME = `${WORK_DIR}/covering.png`;
-const OUT_DIR = 'out';
-
-
+const COVERING_FILE_NAME = withWindowsPath(`${WORK_DIR}/covering.png`);
+const OUT_DIR = "out";
 
 // let inputFile = process.argv[process.argv.indexOf('-i') + 1];
 // if (inputFile === process.argv[0]) return;
 
+function withWindowsPath(path) {
+    return process.platform === "win32" ? path.replace("/", "\\") : path;
+}
+
 function getFileExtension(fileName) {
-    return fileName.split('.').pop();
+    return fileName.split(".").pop();
 }
 
 function getFileNameWithoutExtension(fileName) {
-    return fileName.split('.')[0];
+    return fileName.split(".")[0];
 }
 
 async function getMetadata(fileName) {
     try {
         const metadata = await sharp(fileName).metadata();
         console.log(metadata);
-    }
-    catch (e) {
+    } catch (e) {
         console.error("An Error Occured: ", e);
     }
 }
 
 const loadImage = async (fileName) => {
     try {
-
         return await sharp(fileName);
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
     }
-}
+};
 
 const generateCovering = async (companyName) => {
     const { width, height, left, top } = config[companyName];
-    console.log(config[companyName])
+    console.log(config[companyName]);
     await sharp({
         create: {
             width,
@@ -51,62 +50,68 @@ const generateCovering = async (companyName) => {
             background: {
                 r: 255,
                 g: 255,
-                b: 255
-            }
-        }
+                b: 255,
+            },
+        },
     }).toFile(COVERING_FILE_NAME);
-}
+};
 
 const resizeLogo = async (companyName) => {
     const bareLogo = getFileNameWithoutExtension(LOGO);
     const ext = getFileExtension(LOGO);
-    const newLogoName = `${WORK_DIR}/${bareLogo}-tmp.${ext}`;
+    const newLogoName = withWindowsPath(`${WORK_DIR}/${bareLogo}-tmp.${ext}`);
     const { width, height, left, top } = config[companyName];
-    await sharp(`${WORK_DIR}/${LOGO}`).resize(width, Number.parseInt(height * 0.75), {
-        fit: "contain",
-        position: 'left top',
-        background: {
-            alpha: 1,
-            r: 255,
-            g: 255,
-            b: 255
-        }
-    }).toFile(newLogoName)
+    await sharp(withWindowsPath(`${WORK_DIR}/${LOGO}`))
+        .resize(width, Number.parseInt(height * 0.75), {
+            fit: "contain",
+            position: "left top",
+            background: {
+                alpha: 1,
+                r: 255,
+                g: 255,
+                b: 255,
+            },
+        })
+        .toFile(newLogoName);
     return newLogoName;
-}
+};
 
 const applyNewLogo = async (img, companyName, fileExt) => {
     try {
         const { width, height, left, top } = config[companyName];
         const newLogoName = await resizeLogo(companyName);
-        console.log('New logo', newLogoName);
+        console.log("New logo", newLogoName);
         return await img.composite([
             {
                 input: COVERING_FILE_NAME,
                 left,
-                top
+                top,
             },
             {
                 input: newLogoName,
                 left,
                 top,
-            }
-        ])
+            },
+        ]);
     } catch (e) {
         console.log(e);
         return undefined;
     }
-}
+};
 
 const transferLogo = async (fileName, companyName, outputFileName) => {
     const ext = getFileExtension(fileName);
-    const bareFileName = getFileNameWithoutExtension(fileName)
-    const img = await loadImage(fileName)
+    const bareFileName = getFileNameWithoutExtension(fileName);
+    const img = await loadImage(fileName);
     await generateCovering(companyName);
     const resultImg = await applyNewLogo(img, companyName, ext);
     await resultImg.toFile(outputFileName);
-}
+};
 
 module.exports = {
-    transferLogo, OUT_DIR, WORK_DIR, getFileExtension
-}
+    transferLogo,
+    OUT_DIR,
+    WORK_DIR,
+    getFileExtension,
+    withWindowsPath,
+};
